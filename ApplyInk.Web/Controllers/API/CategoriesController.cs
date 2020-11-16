9c1a1242838998e35fe1ad837e67d6deb5b95cc1
JOHNSR23 <johnsanchez179450@correo.itm.edu.co>
2020-11-16 11:19:18 -0500

    Create CategoriesController

---
+++ ApplyInk.Web/Controllers/API/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplyInk.Web.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplyInk.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProfessions()
+        {
+            return Ok(_context.Categories.OrderBy(p => p.Name));
+        }
+    }
+}
