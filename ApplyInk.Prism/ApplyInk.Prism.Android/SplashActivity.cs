d59beca1bf375ef1b58e804b3de5a98f84e56b3e
YULDER DANIEL QUINTERO HOYOS <yulderquintero205619@correo.itm.edu.co>
2020-11-16 20:45:42 -0500

    Add Splash

--- ApplyInk.Prism/ApplyInk.Prism.Android/SplashActivity.cs
+++ ApplyInk.Prism/ApplyInk.Prism.Android/SplashActivity.cs
@@ -1,19 +1,23 @@
 using Android.App;
 using Android.Content;
+using Android.OS;
 using AndroidX.AppCompat.App;
 
 namespace ApplyInk.Prism.Droid
 {
-    [Activity(Theme = "@style/MainTheme.Splash",
-              MainLauncher = true,
-              NoHistory = true)]
-    public class SplashActivity : AppCompatActivity
+   
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
     {
-        // Launches the startup task
-        protected override void OnResume()
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnResume();
-            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            base.OnCreate(bundle);
+            System.Threading.Thread.Sleep(1800);
+            StartActivity(typeof(MainActivity));
         }
     }
+
+
+
+
 }
