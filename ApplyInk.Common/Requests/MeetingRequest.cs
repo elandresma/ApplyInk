1d6f51231a144b615bf4c2de33fdc653e9fb3975
Andrés Marín Hoyos <Amarinh@intergrupo.com>
2020-11-20 00:09:26 -0500

    Add API create Meetings :DD

--- ApplyInk.Common/Requests/MeetingRequest.cs
+++ ApplyInk.Common/Requests/MeetingRequest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ApplyInk.Common.Enums;
+using ApplyInk.Common.Responses;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -11,6 +13,14 @@ namespace ApplyInk.Common.Requests
         [Required]
         public string Email { get; set; }
 
-        public int IdMeeting { get; set; }
+        public string EmailTattooer { get; set; }
+
+        public int ShopId { get; set; }
+
+        public StatusMeeting Status { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime DateLocal => Date.ToLocalTime();
     }
 }
--- ApplyInk.Web/Controllers/API/MeetingController.cs
+++ ApplyInk.Web/Controllers/API/MeetingController.cs
@@ -1,5 +1,8 @@
 ﻿using ApplyInk.Common.Requests;
+using ApplyInk.Common.Responses;
 using ApplyInk.Web.Data;
+using ApplyInk.Web.Data.Entities;
+using ApplyInk.Web.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -13,16 +16,27 @@ namespace ApplyInk.Web.Controllers.API
     [Route("api/[controller]")]
     public class MeetingController : ControllerBase
     {
+        private readonly IUserHelper _userHelper;
         private readonly DataContext _context;
 
-        public MeetingController(DataContext context)
+        public MeetingController(DataContext context, IUserHelper userHelper)
         {
+            _userHelper = userHelper;
             _context = context;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetMeetings(MeetingRequest request)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Bad request",
+                    Result = ModelState
+                });
+            }
             var myMeeting= await _context.MasterDetailMeetings
                 .Include(m => m.meeting)
                  .Include(m => m.user)
@@ -35,6 +49,68 @@ namespace ApplyInk.Web.Controllers.API
                  .Include(m => m.user)
                  .Where(m => m.meeting.Id== myMeeting.FirstOrDefault().Id)
                  .ToListAsync());
+
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateMeeting ([FromBody] MeetingRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Bad request",
+                    Result = ModelState
+                });
+            }
+
+            Shop shop = await _context.Shops.FindAsync(request.ShopId);
+            Meeting meeting = new Meeting
+            {
+                Date = request.Date,
+                Status = request.Status,
+                Shop = shop
+            };
+
+             await CreateMeetingdetail(meeting);
+
+
+            Meeting meetingClient = _context.Meetings.LastOrDefault();
+            User client = await _userHelper.GetUserAsync(request.Email);
+            MasterDetailMeeting masterDetailMeetingClient = new MasterDetailMeeting
+            {
+                meeting = meetingClient,
+                user = client
+            };
+
+            User tattooer = await _userHelper.GetUserAsync(request.EmailTattooer);
+            MasterDetailMeeting masterDetailMeetingTattooer = new MasterDetailMeeting
+            {
+                meeting = meetingClient,
+                user = tattooer
+            };
+            try
+            {
+                _context.MasterDetailMeetings.Add(masterDetailMeetingClient);
+                _context.MasterDetailMeetings.Add(masterDetailMeetingTattooer);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return Ok(new Response { IsSuccess = true });
+        }
+
+        public async Task<IActionResult> CreateMeetingdetail(Meeting meeting)
+        {
+            _context.Meetings.Add(meeting);
+            await _context.SaveChangesAsync();
+            return Ok();
 
         }
     }
